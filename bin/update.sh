2b4da91a19a8507103cb43f0c66c1eba5b975479
Brian de Heus <me@brian.jp>
2023-10-04 12:42:48 +0900

    Tailwindcss builds by default

--- bin/update.sh
+++ bin/update.sh
@@ -7,7 +7,7 @@ source .venv/bin/activate
 git pull origin master
 
 npm ci
-npx tailwindcss -o ./messageboard/static/messageboard/styles/styles.css --build
+npx tailwindcss -o ./messageboard/static/messageboard/styles/styles.css
 
 ./manage.py migrate
 ./manage.py collectstatic --no-input
