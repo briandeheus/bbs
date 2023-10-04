2b4e4cfdeeec7315c97450a10074ad1359171ef8
Brian de Heus <me@brian.jp>
2023-10-04 12:40:43 +0900

    Adds viewport for mobile and update to update script

--- bin/update.sh
+++ bin/update.sh
@@ -2,7 +2,12 @@
 
 echo "Updating your BBS..."
 
+source .venv/bin/activate
+
 git pull origin master
+
+npm ci
+npx tailwindcss -o ./messageboard/static/messageboard/styles/styles.css --build
 
 ./manage.py migrate
 ./manage.py collectstatic --no-input
