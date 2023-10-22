2cbe1e0aeaca9ffd41a6f7f1278ea9c6cc1931ec
Brian de Heus <me@brian.jp>
2023-10-22 16:27:40 +0400

    Ignore fontawesome stuff we don't use

--- bin/update.sh
+++ bin/update.sh
@@ -12,4 +12,6 @@ npx tailwindcss -o ./messageboard/static/messageboard/styles/styles.css
 pip install -r requirements.txt --extra-index-url "https://dl.fontawesome.com/$FONTAWESOME_TOKEN/fontawesome-pro/python/simple/"
 
 ./manage.py migrate
-./manage.py collectstatic --no-input -v 3
+./manage.py collectstatic --no-input -v3 \
+            --ignore fontawesomepro/svgs/* \
+            --ignore fontawesomepro/js-packages/*
