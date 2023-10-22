26d42e05ccb7770679a1a1f1c17712a4c03abba1
Brian de Heus <me@brian.jp>
2023-10-22 13:42:54 +0400

    Add extra index url outside of requirements file

--- bin/update.sh
+++ bin/update.sh
@@ -9,7 +9,7 @@ git pull origin master
 npm ci
 npx tailwindcss -o ./messageboard/static/messageboard/styles/styles.css
 
-pip install -r requirements.txt
+pip install -r requirements.txt --extra-index-url https://dl.fontawesome.com/$FONTAWESOME_TOKEN/fontawesome-pro/python/simple/
 
 ./manage.py migrate
 ./manage.py collectstatic --no-input
